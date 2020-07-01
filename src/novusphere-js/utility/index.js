9dee6e9adfcbe862c9e1eb165daea7f81f19f09a
asphyxia <asphyxia@novusphere.io>
2020-07-01 12:00:18 -0400

    .

--- src/novusphere-js/utility/index.js
+++ src/novusphere-js/utility/index.js
@@ -146,14 +146,14 @@ function createDOMParser() {
     // hijack the log function for logging to a string variable
     let log = console.log;
     console.enableProxyLog = function (value) {
-        if (window) {
+        if (typeof window !== 'undefined') {
             window._consoleProxyEnabled = value;
             window.localStorage['proxyLog'] = value ? 'enabled' : '';
         }
     }
 
     console.proxyLog = function () {
-        if (window && window._consoleProxyEnabled) {
+        if (typeof window !== 'undefined' && window._consoleProxyEnabled) {
             const args = Array.from(arguments).map(a => JSON.stringify(a));
             window._consoleProxy = (window._consoleProxy || '') + [`[${new Date().toLocaleTimeString()}]`, ...args].join(' ') + '\r\n';
         }
@@ -163,7 +163,7 @@ function createDOMParser() {
         log.apply(this, arguments);
     }
 
-    if (window && window.localStorage['proxyLog']) {
+    if (typeof window !== 'undefined' && window.localStorage['proxyLog']) {
         console.enableProxyLog(true);
     }
 
