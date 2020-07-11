3f10984c8606089f63a4d698744830958673116f
asphyxia <asphyxia@novusphere.io>
2020-07-11 14:21:58 -0400

    add follower count to UserProfileLink.vue popover

--- src/components/UserProfileLink.vue
+++ src/components/UserProfileLink.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <v-menu
-      max-width="400"
-      v-model="showPopover"
-      :position-x="popoverX"
-      :position-y="popoverY"
-    >
-      <UserProfileCard v-if="showPopover" :displayName="displayName" :publicKey="publicKey" :uidw="uidw" small />
+    <v-menu max-width="400" v-model="showPopover" :position-x="popoverX" :position-y="popoverY">
+      <UserProfileCard
+        v-if="showPopover"
+        :displayName="displayName"
+        :publicKey="publicKey"
+        :uidw="uidw"
+        small
+      >
+        <span class="d-block text-center">{{ followers }} followers</span>
+      </UserProfileCard>
     </v-menu>
     <v-btn text v-if="btn" :to="link">
       <PublicKeyIcon v-show="!noIcon" :publicKey="publicKey" />
@@ -31,6 +34,9 @@
 <script>
 import PublicKeyIcon from "@/components/PublicKeyIcon";
 import UserProfileCard from "@/components/UserProfileCard";
+import {
+  getUserProfile
+} from "@/novusphere-js/discussions/api";
 
 export default {
   name: "UserProfileLink",
@@ -41,11 +47,10 @@ export default {
   props: {
     noIcon: Boolean,
     displayName: String,
-    uidw: String,
     publicKey: String,
     big: Boolean,
     btn: Boolean,
-    popover: { type: Boolean, default: true }
+    popover: { type: Boolean, default: false }
   },
   computed: {
     link() {
@@ -55,7 +60,9 @@ export default {
   data: () => ({
     showPopover: false,
     popoverX: 0,
-    popoverY: 0
+    popoverY: 0,
+    uidw: "",
+    followers: 0
   }),
   methods: {
     async clicked(e) {
@@ -66,10 +73,19 @@ export default {
         return;
       }
 
+      const info = await getUserProfile(this.publicKey);
+
+      this.uidw = info.uidw;
+      //this.publicKey = publicKey;
+      //this.displayName = info.displayName;
+      this.followers = info.followers;
+      //this.posts = info.posts;
+      //this.threads = info.threads;
+
       const rect = this.$el.getBoundingClientRect();
-      this.showPopover = true;
       this.popoverX = rect.x + rect.width + 10;
       this.popoverY = rect.y - 10;
+      this.showPopover = true;
     }
   }
 };
