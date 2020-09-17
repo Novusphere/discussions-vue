888b9fc84c5f977462028906fd1f965e9ed8215e
asphyxia <asphyxia@novusphere.io>
2020-09-17 15:36:16 -0400

    Update landing with info provided by Paul

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -18,6 +18,83 @@
         </div>
       </template>
     </LandingInfo>
+    <LandingInfo>
+      <template v-slot:main>
+        <h1 class="mb-4">Getting Starting</h1>
+        <span>no description provided</span>
+      </template>
+      <template v-slot:info="{ reveal }">
+        <div v-if="reveal" class="mt-4 mb-4 ml-4 mr-4">
+          <p>
+            <a
+              href="https://discussions.app/tag/faq/quvwb1feu9hg/faq-working-guide-to-discussions-app"
+              target="_blank"
+            >FAQ</a>
+          </p>
+        </div>
+        <div v-else>
+          <v-icon color="primary" size="256px">engineering</v-icon>
+        </div>
+      </template>
+    </LandingInfo>
+    <LandingInfo>
+      <template v-slot:main>
+        <h1 class="mb-4">Get ATMOS</h1>
+        <span>no description provided</span>
+      </template>
+      <template v-slot:info="{ reveal }">
+        <div v-if="reveal" class="mt-4 mb-4 ml-4 mr-4">
+          <p>
+            <v-list>
+              <v-list-item>
+                <a
+                  href="https://www.probit.com/app/exchange/ATMOS-BTC"
+                  target="_blank"
+                >Probit - ATMOS/BTC</a>
+              </v-list-item>
+              <v-list-item>
+                <a
+                  href="https://newdex.io/kline/novusphereio-atmos-eos"
+                  target="_blank"
+                >NewDex - ATMOS/EOS</a>
+              </v-list-item>
+            </v-list>
+          </p>
+        </div>
+        <div v-else>
+          <v-icon color="primary" size="256px">trending_up</v-icon>
+        </div>
+      </template>
+    </LandingInfo>
+    <LandingInfo>
+      <template v-slot:main>
+        <h1 class="mb-4">About Us</h1>
+        <span>no description provided</span>
+      </template>
+      <template v-slot:info="{ reveal }">
+        <div v-if="reveal" class="mt-4 mb-4 ml-4 mr-4">
+          <p>
+            <v-list>
+              <v-list-item>
+                <a href="https://discussions.app/u/xia256-WFSe/" target="_blank">xia256</a>
+              </v-list-item>
+              <v-list-item>
+                <a href="https://discussions.app/u/Jacques-nYYH/" target="_blank">Jacques</a>
+              </v-list-item>
+              <v-list-item>
+                <a href="https://discussions.app/u/Brian_Novusphere-cpjV/" target="_blank">Brian</a>
+              </v-list-item>
+              <v-list-item>
+                <a href="https://discussions.app/u/Novusphere_Paul-Dx5o/" target="_blank">Paul</a>
+              </v-list-item>
+            </v-list>
+          </p>
+        </div>
+        <div v-else>
+          <v-icon color="primary" size="256px">groups</v-icon>
+        </div>
+      </template>
+    </LandingInfo>
   </div>
 </template>
 
